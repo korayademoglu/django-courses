67a168c05ab662ef3d40103afcace7569fad84ff
korayademoglu <korayademoglu@gmail.com>
2023-11-05 21:35:46 +0300

    html template test

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('kurslar',views.kurslar),
+    path('',views.home),
+    path('test',views.test),
     path('youtube', views.youtubeLink),
     path('<cat>', views.getCoursesByCategory)
 ]
--- courses/views.py
+++ courses/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 
 def home(req):
-    return HttpResponse('anasayfa')
+    return HttpResponse('<h1>kurslar anasayfa</h1>')
 def kurslar(req):
     return HttpResponse('kurs listesi')
+def test(req):
+    return render(req,'test.html')
 
 def getCoursesByCategory(req,cat):
     return HttpResponse(f'{cat} sayfasını getir')
